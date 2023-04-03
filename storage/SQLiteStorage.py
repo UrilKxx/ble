79e1391610b3e31ac9c5e6da0aa9b6aa03af1f33
urikxx <urikxx@loc.loc>
2023-04-03 23:59:51 +0300

    renew query

--- storage/SQLiteStorage.py
+++ storage/SQLiteStorage.py
@@ -34,8 +34,8 @@ class SQLiteStorage(Storage):
 
     def __init__(self, db: str = 'devices.db'):
         self._db = db
-        # if self._db not in os.listdir():
-        #     self.create_db()
+        if self._db not in os.listdir():
+            self.create_db()
 
     @property
     def db(self) -> str:
@@ -44,32 +44,12 @@ class SQLiteStorage(Storage):
     @con_db
     def create_db(self, connection):
         cursor = connection.cursor()
-        query = '''
-    create table 'devices'
-    (
-        mac text not null unique unique,
-        avg_battery REAL default 0,
-        avg_temp     REAL    default 0,
-        avg_humidity REAL    default 0,
-        online       INTEGER default 0 not null
-    );
+        cursor.execute(''' create table 'devices' ( mac text not null unique unique, avg_battery REAL default 0,
+                avg_temp REAL default 0, avg_humidity REAL default 0, online INTEGER default 0 not null);''')
 
-    create table statistic_data
-    (
-        divice_mac  TEXT not null
-            constraint statistic_data_devices_mac_fk
-                references devices (mac),
-        time        TEXT not null,
-        temperature REAL not null,
-        humidity    real not null,
-        battery     REAL not null
-    );
-
-    create unique index statistic_data_divice_mac_uindex
-        on statistic_data (divice_mac);
-
-        '''
-        cursor.execute(query)
+        cursor.execute('''create table statistic_data ( id integer not null primary key autoincrement unique,
+               divice_mac TEXT not null constraint statistic_data_devices_mac_fk references devices (mac), time TEXT not null,
+              temperature REAL not null, humidity real not null, battery REAL not null);''')
 
     @con_db
     def get_devices(self, connection) -> List[Device]:
@@ -120,14 +100,15 @@ class SQLiteStorage(Storage):
                            humidity: float = 0, battery: float = 0, connection=None):
         cursor = connection.cursor()
         cursor.execute(
-            f"INSERT INTO 'statistic_data' (divice_mac, time, temperature, humidity, battery) VALUES ('{mac}', '{time.ctime()}', {temperature}, {humidity}, {battery})")
+            f"INSERT INTO 'statistic_data' (divice_mac, time, temperature, humidity, battery)"
+            f" VALUES ('{mac}', '{time.ctime()}', {temperature}, {humidity}, {battery})")
         connection.commit()
 
     @con_db
     def get_statistic_data(self, mac: str, connection=None):
         statistic_data = []
         cursor = connection.cursor()
-        data = cursor.execute("SELECT * FROM 'statistic_data'")
+        data = cursor.execute(f"SELECT * FROM 'statistic_data' where divice_mac = '{mac}'")
         for row in data:
             statistic_data.append(row)
         return statistic_data
