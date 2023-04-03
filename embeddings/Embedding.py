13d26d0a3483a8a3e34ec9ed508334ae7c425d82
urikxx <urikxx@loc.loc>
2023-04-03 23:57:09 +0300

    add update devices data threading & add statistic data

--- embeddings/Embedding.py
+++ embeddings/Embedding.py
@@ -1,15 +1,22 @@
-from typing import List
+import threading
+import time
 
+from logger import logger as logger
+from btlewrap.bluepy import BluepyBackend
+from bluepy.btle import BTLEException
+from typing import List
 from bluepy.btle import Scanner
 
-from storage.Device import Device
+from mitemp.mitemp_bt.mitemp_bt_poller import MiTempBtPoller
+from mitemp.mitemp_bt.mitemp_bt_poller import MI_TEMPERATURE, MI_HUMIDITY, MI_BATTERY
+
 from scanner.ScanDelegate import ScanDelegate
+from storage.Device import Device
 from storage.SQLiteStorage import SQLiteStorage
 from storage.Storage import Storage
 
 logger = logger.get_logger(__name__)
 
-# from bluepy.btle import BTLEException
 
 class Embedding(object):
     def __init__(self, storage: Storage = SQLiteStorage(), timeout: int = 5):
@@ -17,10 +24,51 @@ class Embedding(object):
         self._storage: Storage = storage
         self._timeout = timeout
         self._scanner = Scanner().withDelegate(ScanDelegate())
+        thr = threading.Thread(target=self.update_devices_data, daemon=True, name='update_devices_data')
+        thr.start()
 
-    # def check_devices(self):
-    #     for d in self._storage.get_devices():
-    #         self._pollers.append(MiTempBtPoller(d.mac, BluepyBackend, self._timeout))
+    @property
+    def storage(self):
+        return self._storage
+
+    def update_devices_data(self, timeout=10):
+        logger.info(f"Start thread update devices data")
+        while True:
+            for d in self._storage.get_devices():
+                self.get_device_info(d)
+                self._storage.update_device(d)
+            time.sleep(timeout)
+
+    def get_device_info(self, device: Device):
+        poller = MiTempBtPoller(device.mac, BluepyBackend, ble_timeout=self._timeout)
+        try:
+            temperature = poller.parameter_value(MI_TEMPERATURE)
+            humidity = poller.parameter_value(MI_HUMIDITY)
+            battery = poller.parameter_value(MI_BATTERY)
+            logger.info(f"Statistic data from device "
+                        f"{device.mac} - temperature {temperature} - humidity {humidity} - battery {battery}")
+            self._storage.add_statistic_data(device.mac, temperature, humidity, battery)
+            stat_data = self.get_stat(device)
+            avg_temperature = 0
+            avg_humidity = 0
+            avg_battery = 0
+            for d in stat_data:
+                avg_temperature += d[3]
+                avg_humidity += d[4]
+                avg_battery += d[5]
+            device.avg_temperature = avg_temperature / len(stat_data)
+            device.avg_humidity = avg_humidity / len(stat_data)
+            device.avg_battery = avg_battery / len(stat_data)
+            device.is_online = True
+            self._storage.update_device(device)
+        except BTLEException as e:
+            logger.error(f"Error connecting to device {device.mac}: {str(e)}")
+            self._storage.update_online_device(device, False)
+        except Exception as e:
+            logger.error(f"Error polling device {device.mac}. Device might be unreachable or offline.")
+            self._storage.update_online_device(device, False)
+        finally:
+            ...
 
     def scan_devices(self) -> List:
         logger.info(f"Start scan devices ...")
@@ -43,3 +91,6 @@ class Embedding(object):
 
     def get_device(self, mac) -> Device:
         return self._storage.get_device(mac)
+
+    def get_stat(self, device: Device) -> List:
+        return self._storage.get_statistic_data(device.mac)
