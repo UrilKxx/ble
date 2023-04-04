8302572e1ce51d94389be2f10142910b82445626
urikxx <urikxx@loc.loc>
2023-04-04 22:31:26 +0300

    new response & delete_device method

--- flask_server.py
+++ flask_server.py
@@ -39,8 +39,16 @@ def create_app():
         mac = args.get('mac')
         device = embeddings.add_device(mac)
         if not isinstance(device, Device):
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        return json.loads(str(device))
+            return Response(str(device), status=status.HTTP_400_BAD_REQUEST)
+        return json.loads(str(device)), status.HTTP_201_CREATED
+
+    @app.route('/devices/<mac>', methods=['DELETE'])
+    def delete_device(mac: str):
+        device = embeddings.get_device(mac)
+        if device is not None:
+            if embeddings.delete_device(device):
+                return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
     @app.route('/devices/online', methods=['GET'])
     def get_online_device():
